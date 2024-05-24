99574255fe4741e151eab2046d18a22cbd72e352
千葉 智貴 <132980385+kentem-to-chiba@users.noreply.github.com>
2024-05-24 15:27:53 +0900

    faviconをdocumentから読むように

---
+++ pages/_document.js
@@ -0,0 +1,15 @@
+import { Head, Html, Main, NextScript } from 'next/document'
+
+const Document = () => (
+  <Html lang="ja">
+    <Head>
+      <link rel="icon" sizes="32x32" href="/favicon.ico" />
+    </Head>
+    <body>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+)
+
+export default Document
