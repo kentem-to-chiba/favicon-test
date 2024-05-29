9a9d7535b51eb5bbf3f488c04d556089bbcc6e5c
千葉 智貴 <132980385+kentem-to-chiba@users.noreply.github.com>
2024-05-29 11:25:18 +0900

    icoを削除してiPadでファビコンが表示されるか確認したい

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,6 @@ import { Head, Html, Main, NextScript } from 'next/document'
 const Document = () => (
   <Html lang="ja">
     <Head>
-      <link rel="icon" sizes="32x32" href="/favicon.ico" />
       <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
     </Head>
     <body>
